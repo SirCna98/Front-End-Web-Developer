1ff1d797af3281f20d36715551f89f4b8df31c5b
NavidAdib98 <navid.adib.98@gmail.com>
2022-08-20 14:00:33 +0430

    add java-script.js

---
+++ java-script.js
@@ -0,0 +1,69 @@
+<script src="..."></script>
+// --------------------------------------------------------
+// for testing
+document.write("Hello");
+// --------------------------------------------------------
+
+window.alert("Hello");  // 
+alert("aaa");
+// --------------------------------------------------------
+// for debugging
+console.log("aa");
+// --------------------------------------------------------
+let a = 1;
+
+// The let keyword was introduced in ES6 (2015).
+
+// Variables defined with let cannot be Redeclared.
+
+// Variables defined with let must be Declared before use.
+
+// Variables defined with let have Block Scope.
+// --------------------------------------------------------
+// You can create a constant array:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// You can change an element:
+cars[0] = "Toyota"; 
+
+// Use const when you declare:
+
+// A new Array
+// A new Object
+// A new Function
+// A new RegExp
+// --------------------------------------------------------
+// typeof	Returns the type of a variable
+// --------------------------------------------------------
+// ===	equal value and equal type
+// ==	equal to
+
+// --------------------------------------------------------
+
+// HTML Events
+// An HTML event can be something the browser does, or something a user does.
+
+// Here are some examples of HTML events:
+
+// An HTML web page has finished loading
+// An HTML input field was changed
+// An HTML button was clicked
+
+// Event	      Description
+// onchange	      An HTML element has been changed
+// onclick	      The user clicks an HTML element
+// onmouseover	  The user moves the mouse over an HTML element
+// onmouseout	  The user moves the mouse away from an HTML element
+// onkeydown	  The user pushes a keyboard key
+// onload	      The browser has finished loading the page
+// --------------------------------------------------------
+// ( new )
+// Comparing two JavaScript objects always returns false.
+
+// --------------------------------------------------------
+// string methods
+// slice(start, end)
+// substring(start, end)
+// substr(start, length)
+// replace(searchValue: string, replaceValue: string)
+
